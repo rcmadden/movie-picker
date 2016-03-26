088eb49397edff6beca94efa5ea3741a36cca873
Dan Connolly <dckc@madmode.com>
2016-03-26 12:58:22 -0500

    we have a prompt for 1 movie!
      - start to use log.debug()
      - get raw_input from python3 code module
      - misc PEP8 formatting
      - prune ascii-art comments

--- movies.py
+++ movies.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import json
 import random
 from urllib.request import urlopen
@@ -5,20 +6,25 @@ from urllib.parse import quote
 # import urllib
 # import urllib.request
 # from urllib.parse import urlparse
-import pprint
+import logging
+from pprint import pformat
+import code
+
+log = logging.getLogger(__name__)
 
 DEFAULT_CATEGORY = "American_science_fiction_action_films"
-WIKIPEDIA_CATEGORY_URL = "https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:{}&format=json&cmlimit=250&cmcontinue={}"
-OMDBAPI_TITLE_URL = "http://www.omdbapi.com/?t={}&y=&plot=short&r=json&tomatoes=true"
+WIKIPEDIA_CATEGORY_URL = "https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:{}&format=json&cmlimit=250&cmcontinue={}"  # noqa
+OMDBAPI_TITLE_URL = "http://www.omdbapi.com/?t={}&y=&plot=short&r=json&tomatoes=true"  # noqa
 
-#have the user pick this many movies before quitting
+# have the user pick this many movies before quitting
 NUM_MOVIES = 3
 
-#use this to build the URL to the movie on IMDB, e.g.: http://www.imdb.com/title/tt0093773
+# use this to build the URL to the movie on IMDB, e.g.: http://www.imdb.com/title/tt0093773  # noqa
 IMDB_URL = "http://www.imdb.com/title/{}"
 
-## API code ###################################################################
+# API code ##################################################################
 # https://www.mediawiki.org/wiki/API:Main_page
+
 
 def clean_title(title):
     '''
@@ -74,6 +80,7 @@ def fetch_wikipedia_titles(category):
         cmcontinue = data['continue']['cmcontinue']
     return filter_titles(out)
 
+
 def fetch_omdb_info(title):
     '''
     Retrieve movie information from OMDb API's title search. and encode url spaces https://docs.python.org/3/library/urllib.parse.html
@@ -93,13 +100,12 @@ def fetch_omdb_info(title):
     return data
 
 
-## classes ####################################################################
-
 class Movie(object):
     '''
     Represents a movie with data from the omdbapi.com.
     '''
     def __init__(self, data):
+        log.debug('movie data keys: %s', data.keys())
         self.data = data
 
     @property
@@ -107,40 +113,46 @@ class Movie(object):
         return self.data['Title']
 
     def __str__(self):
-        #TODO: Move your formatting code from ex3.py into this __str__ method.
-        #When you `print` an object, its __str__ method (if it has one) is called to
-        #turn the object into a string. Instead of directly printing it, just build
-        #up the string and return it. Note that str needs to return a bytestring, while
-        #the strings inside self.data are unicode, so we use encode('utf8') before returning
-        #the result.
-        txt = u"Title: {}".format(self.data['Title'])
-        return txt.encode('utf8')
+        # TODO: Move your formatting code from ex3.py into this
+        # __str__ method.  When you `print` an object, its __str__
+        # method (if it has one) is called to turn the object into a
+        # string. Instead of directly printing it, just build up the
+        # string and return it. Note that str needs to return a
+        # bytestring, while the strings inside self.data are unicode,
+        # so we use encode('utf8') before returning the result.
+        return u"Title: {Title} {Year} {Rated}".format(**self.data)
+
 
 class MoviePicker(object):
-    '''
-    Random movie picker functionality. Returns random movies and keeps track of picked movies.
+    '''Random movie picker functionality. Returns random movies and keeps
+    track of picked movies.
+
     '''
     def __init__(self, titles):
+        '''Initializes the MoviePicker with a list of `titles` that will be
+        picked from randomly.
+
         '''
-        Initializes the MoviePicker with a list of `titles` that will be picked from randomly.
-        '''
-        # self.titles = random.sample(titles, len(titles))
-        # self.picked = []
-        pass
+        self.titles = titles
+        self.picked = []
 
     def get_random_movie(self):
+        '''Pick a random title, fetch its data from OMDB, and return it as a
+        Movie object.
+
         '''
-        Pick a random title, fetch its data from OMDB, and return it as a Movie object.
-        '''
-        #TODO: Move your "get a random movie" logic from ex3.py into this method. Instead of
-        #picking from the JSON, you will pick from self.titles, then call `fetch_omdb_info`
-        #to get the movie data, pass that data into Movie() to turn it into a Movie object,
-        #the return that object.
-        pass
+        # TODO: Move your "get a random movie" logic from ex3.py into
+        # this method. Instead of picking from the JSON, you will pick
+        # from self.titles, then call `fetch_omdb_info` to get the
+        # movie data, pass that data into Movie() to turn it into a
+        # Movie object, the return that object.
+        title = random.choice(self.titles)
+
+        return Movie(fetch_omdb_info(title))
 
     def add_to_list(self, m):
         '''Add the given Movie object `m` to the list of picked movies.'''
-        #TODO: just append the given Movie object to self.picked
+        # TODO: just append the given Movie object to self.picked
         pass
 
     def get_list(self):
@@ -148,30 +160,33 @@ class MoviePicker(object):
         #TODO: return `m.title` for each of the movies in self.picked
         pass
 
-## main #######################################################################
 
 def main():
+    raw_input = code.InteractiveConsole().raw_input
+
     # picker = MoviePicker(fetch_wikipedia_titles(DEFAULT_CATEGORY))
     # return picker
     # fetch_wikipedia_titles(DEFAULT_CATEGORY)
     wiki_titles = fetch_wikipedia_titles(DEFAULT_CATEGORY)
-    return fetch_omdb_info(wiki_titles[0])
 
-    # pprint.pprint(wiki_titles)
-    # pprint.pprint(wiki_tites)
-    # num_picked = 0
-    # while num_picked < NppUM_MOVIES:
-    #     movie = picker.get_random_movie()
-    #     print(movie)
-    #     answer = raw_input("Add movie to your list? ")
-    #     if answer.lower().startswith('y'):
-    #         picker.add_to_list(movie)
-    #         num_picked += 1
-    # print("\n== Your movies ==\n")
-    # for title in picker.get_list():
-    #     print(title)
+    log.debug('titles: %s', pformat(wiki_titles))
+
+    picker = MoviePicker(wiki_titles)
+
+    num_picked = 0
+    while num_picked < NUM_MOVIES:
+        movie = picker.get_random_movie()
+        print(movie)
+        answer = raw_input("Add movie to your list? ")
+        if answer.lower().startswith('y'):
+            picker.add_to_list(movie)
+            num_picked += 1
+    print("\n== Your movies ==\n")
+    for title in picker.get_list():
+        print(title)
 
 if __name__ == "__main__":
-    #TODO: (bonus) Use sys.argv to get a category name on the command line (but
-    #defaulting to DEFAULT_CATEGORY if nothing is passed in)
+    logging.basicConfig(level=logging.DEBUG)
+    # TODO: (bonus) Use sys.argv to get a category name on the command
+    # line (but defaulting to DEFAULT_CATEGORY if nothing is passed in)
     main()
